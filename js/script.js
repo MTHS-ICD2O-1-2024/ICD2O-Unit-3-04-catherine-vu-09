5f318f083ceb9684f00156cbe374e17cd4c45043
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-05-25 16:23:12 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine Vu
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateCelcius() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+
+  // process
+  const celcius = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('temperature').innerHTML = 'The temperature in celcius is: ' + celcius + '°'
+}
